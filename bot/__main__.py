d5c24f8648e370e24bebb1fb640b34c0259f0398
abhisek334 <101055702+abhisek334@users.noreply.github.com>
2022-05-14 19:44:23 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -18,7 +18,7 @@ from .helper.telegram_helper.message_utils import sendMessage, sendMarkup, editM
 from .helper.telegram_helper.filters import CustomFilters
 from .helper.telegram_helper.button_build import ButtonMaker
 
-from .modules import authorize, list, cancel_mirror, mirror_status, mirror, clone, watch, shell, eval, delete, count, leech_settings, search, rss
+from .modules import authorize, list, cancel_mirror, mirror_status, mirror, clone, watch, shell, delete, count, leech_settings
 
 
 
