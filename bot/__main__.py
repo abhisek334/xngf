de0d63ad93e64fa32436f9a741b3e084d6bdc78e
abhisek334 <101055702+abhisek334@users.noreply.github.com>
2022-06-08 22:32:24 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -70,8 +70,8 @@ def start(update, context):
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
-This bot can mirror all your links to Google Drive!
-Type /{BotCommands.HelpCommand} to get a list of available commands
+MIRROR BOT POWERD BY @XdaAbhi🔥 DEPLOYED ON AWS!
+Type /{BotCommands.HelpCommand} to get a list of service list
 '''
         sendMarkup(start_string, context.bot, update.message, reply_markup)
     else:
